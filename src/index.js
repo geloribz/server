b39319b301a78dcbe7c46955ba83ac7e0758c376
angeloribleza <103402008+geloribz@users.noreply.github.com>
2023-08-30 15:13:43 +0800

    setting up

---
+++ src/index.js
@@ -0,0 +1,17 @@
+//will serve everything to ../server/server.js
+
+const express = require("express");
+const app = express();
+const { PORT } = require("./constants");
+
+const appStart = () => {
+  try {
+    app.listen(PORT, () => {
+      console.log("The app is running at http://localhost:${PORT}");
+    });
+  } catch (error) {
+    console.log("Error: ${error.message}");
+  }
+};
+
+appStart();
